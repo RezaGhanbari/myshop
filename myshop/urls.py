8977c919cba39e42ab67ec7e2e9e02510b1d6a1d
Reza <rezapython@gmail.com>
2016-11-11 18:32:00 +0330

    Creating catalog templates

--- myshop/urls.py
+++ myshop/urls.py
@@ -1,7 +1,18 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('shop.urls', namespace='shop')),
 ]
+
+# Remember that we only serve static files this way during the development.
+# In a production environment, you should never serve
+# static files with Django.
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
